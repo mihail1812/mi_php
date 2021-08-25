721ebf07398b8b23c597479b5be23583095e4129
Mihail <vahturov.m@yandex.ru>
2021-08-25 17:45:23 +0400

    add operators.php

---
+++ operators.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h3>Арифметические операторы</h3>
+    <p>Арифметические операторы применяются для выполнения действий с числами.</p>
+    <p><span style="color: #000080;">+</span>&nbsp;&nbsp;&nbsp;&nbsp;— оператор сложения;<br>
+<span style="color: #000080;">—</span>&nbsp;&nbsp;&nbsp;&nbsp;— оператор вычитания;<br>
+<span style="color: #000080;">/</span>&nbsp;&nbsp;&nbsp;&nbsp;— оператор деления;<br>
+<span style="color: #000080;">*</span>&nbsp;&nbsp;&nbsp;&nbsp;— оператор умножения;<br>
+<span style="color: #000080;">%</span>&nbsp;&nbsp;&nbsp;&nbsp;— оператор получения остатка при делении;<br>
+<span style="color: #000080;">++</span>&nbsp;&nbsp;&nbsp;&nbsp;— оператор увеличения на единицу (инкремент);<br>
+<span style="color: #000080;">— —</span>&nbsp;&nbsp;&nbsp;&nbsp;— оператор уменьшения на единицу (декремент)</p>
+
+
+</body>
+</html>
