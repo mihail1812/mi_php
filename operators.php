1ad1ac6e3d15784f1165bec383b460c00e289cb7
Mihail <vahturov.m@yandex.ru>
2021-08-25 23:03:17 +0400

    add operators and fancybox

--- operators.php
+++ operators.php
@@ -1,22 +1,56 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <h3>Арифметические операторы</h3>
     <p>Арифметические операторы применяются для выполнения действий с числами.</p>
     <p><span style="color: #000080;">+</span>&nbsp;&nbsp;&nbsp;&nbsp;— оператор сложения;<br>
-<span style="color: #000080;">—</span>&nbsp;&nbsp;&nbsp;&nbsp;— оператор вычитания;<br>
-<span style="color: #000080;">/</span>&nbsp;&nbsp;&nbsp;&nbsp;— оператор деления;<br>
-<span style="color: #000080;">*</span>&nbsp;&nbsp;&nbsp;&nbsp;— оператор умножения;<br>
-<span style="color: #000080;">%</span>&nbsp;&nbsp;&nbsp;&nbsp;— оператор получения остатка при делении;<br>
-<span style="color: #000080;">++</span>&nbsp;&nbsp;&nbsp;&nbsp;— оператор увеличения на единицу (инкремент);<br>
-<span style="color: #000080;">— —</span>&nbsp;&nbsp;&nbsp;&nbsp;— оператор уменьшения на единицу (декремент)</p>
+        <span style="color: #000080;">—</span>&nbsp;&nbsp;&nbsp;&nbsp;— оператор вычитания;<br>
+        <span style="color: #000080;">/</span>&nbsp;&nbsp;&nbsp;&nbsp;— оператор деления;<br>
+        <span style="color: #000080;">*</span>&nbsp;&nbsp;&nbsp;&nbsp;— оператор умножения;<br>
+        <span style="color: #000080;">%</span>&nbsp;&nbsp;&nbsp;&nbsp;— оператор получения остатка при делении;<br>
+        <span style="color: #000080;">++</span>&nbsp;&nbsp;&nbsp;&nbsp;— оператор увеличения на единицу (инкремент);<br>
+        <span style="color: #000080;">— —</span>&nbsp;&nbsp;&nbsp;&nbsp;— оператор уменьшения на единицу (декремент)
+    </p>
 
+    <h3>Поразрядные операторы</h3>
+    <p>Поразрядные операторы предназначены для работы с двоичными данными. Если кто-то не представляет что это такое — объясню. Двоичные числа — это числа типа 1001000011100000111000.</p>
+    <p><span style="color: #000080;">&amp;</span>&nbsp;&nbsp;&nbsp;&nbsp;— поразрядное соединение И (and);<br>
+        <span style="color: #000080;">~</span>&nbsp;&nbsp;&nbsp;&nbsp;— поразрядное отрицание (not);<br>
+        <span style="color: #000080;">|</span>&nbsp;&nbsp;&nbsp;&nbsp;— поразрядное объединение ИЛИ (or);<br>
+        <span style="color: #000080;">^</span>&nbsp;&nbsp;&nbsp;&nbsp;— поразрядное исключение ИЛИ (xor);<br>
+        <span style="color: #000080;">&lt;&lt;</span>&nbsp;&nbsp;&nbsp;&nbsp;— сдвиг влево битового значения операнда;<br>
+        <span style="color: #000080;">&gt;&gt;</span>&nbsp;&nbsp;&nbsp;&nbsp;— сдвиг вправо битового значения операнда;
+    </p>
 
+    <h3>Операторы сравнения</h3>
+    <p><span style="color: #000080;">&gt;</span> &nbsp;&nbsp;&nbsp;&nbsp;— оператор больше;<br>
+        <span style="color: #000080;">=&gt;</span> &nbsp;&nbsp;&nbsp;&nbsp;— оператор больше или равно;<br>
+        <span style="color: #000080;">&lt; </span> &nbsp;&nbsp;&nbsp;&nbsp;— оператор меньше;<br>
+        <span style="color: #000080;">&lt;=</span> &nbsp;&nbsp;&nbsp;&nbsp;— оператор меньше или равно;<br>
+        <span style="color: #000080;">== </span>&nbsp;&nbsp;&nbsp;&nbsp;— оператор равенство;<br>
+        <span style="color: #000080;">!= </span>&nbsp;&nbsp;&nbsp;&nbsp;— оператор неравенство;<br>
+        <span style="color: #000080;">=== </span>&nbsp;&nbsp;&nbsp;&nbsp;— оператор эквивалентности (равны значение и тип переменной);<br>
+        <span style="color: #000080;">!== </span>&nbsp;&nbsp;— оператор неэквивалентности;
+    </p>
+
+    <h3>Логические операторы</h3>
+    <p><span style="font-family:Consolas;">&amp;&amp;</span> &nbsp;&nbsp;&nbsp;&nbsp;— соединительный союз И;<br>
+        <span style="font-family:Consolas;">and</span> &nbsp;&nbsp;&nbsp;&nbsp;— тоже И, но с меньшим приоритетом;<br>
+        <span style="font-family:Consolas;">||</span> &nbsp;&nbsp;&nbsp;&nbsp;— разделительный союз ИЛИ;<br>
+        <span style="font-family:Consolas;">or</span> &nbsp;&nbsp;&nbsp;&nbsp;— тоже ИЛИ, но с меньшим приоритетом;<br>
+        <span style="font-family:Consolas;">xor</span> &nbsp;&nbsp;&nbsp;&nbsp;— исключающее один элемент ИЛИ;<br>
+        <span style="font-family:Consolas;">!</span> &nbsp;&nbsp;&nbsp;&nbsp;— отрицание;
+    </p>
+
+    
 </body>
+
 </html>
